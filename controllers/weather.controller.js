305fec5064e2178f2ff4fa9675b5da4865aac625
Anup Nautiyal <anupn8083@gmail.com>
2026-01-10 21:33:05 +0530

    Controller and Routes updated

--- controllers/weather.controller.js
+++ controllers/weather.controller.js
@@ -1,7 +1,28 @@
-const getWeather = (req, res) => {
-    res.render('index.ejs', { weather: null, error: null });
-}
+import weatherService from "../services/weather.service.js";
+
+const handleWeatherRequest = async (req, res) => {
+const city = req.query.city;
+  if (!city) {
+    return res.render("index", {
+      weather: null,
+      error: null
+    });
+  }
+  try {
+    const weatherData = await weatherService.getWeatherByCity(city);
+
+    res.render("index", {
+      weather: weatherData,
+      error: null
+    });
+  } catch (error) {
+    res.render("index", {
+      weather: null,
+      error: "City not found. Please try again."
+    });
+  }
+};
 
 export default {
-    getWeather
+  handleWeatherRequest
 };
--- routes/weather.routes.js
+++ routes/weather.routes.js
@@ -2,6 +2,6 @@ import express from "express";
 import weatherController from "../controllers/weather.controller.js";
 
 const router = express.Router();
-router.get("/", weatherController.getWeather);
+router.get("/", weatherController.handleWeatherRequest);
 
 export default router;
